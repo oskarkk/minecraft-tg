8f2a3f090c6e175eb6f83ba0107e6194fd6a75dc
oskarkk <oskarkalic@gmail.com>
2018-02-26 04:46:00 +0100

    Update tg.sh

--- tg.sh
+++ tg.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-# adres bot API tg
-tgURL=''
-# adres pliku logu serwera
-mclog='/home/mcserver/spigot/logs/latest.log'   # adres pliku logu serwera
+# Telegram bot's token
+botToken=""
+# spigot directory absolute path with slash at the end
+spigot="/home/mcserver/spigot/"
+# user who will be able to send commands
+adminUsername=""
+# ID of chat which will get messages from Minecraft (user or group)
+chatID="-244449217"
+# ID of chat which will get the entire server log (user or group)
+adminChatID=""
 
-cd /home/mcserver/tg
+
+# bot API URL
+tgURL="https://api.telegram.org/bot"$botToken"/"
+# spigot log file relative path
+mclog="logs/latest.log"
 
 
+cd $spigot
+mkdir -p tg
 
+
+# infinite loop
 while true
 do
 
@@ -16,97 +30,96 @@ do
 
 
 #
-# przesylanie z serwera na telegram
+# Spigot server -> Telegram
 #
 
+# copy latest log file
+cp $mclog tg/current.log
+# leave only lines that aren't in the previous log copy and remove color formatting
+awk 'NR==FNR{a[$0]=1;next}!a[$0]' tg/last.log tg/current.log | sed 's/[\x1B][^m]*m//g' > tg/temp.log
 
-# kopiujemy obecny log serwera
-cp $mclog current.log
-# wybieramy z niego wszystkie linie ktorych wczesniej nie bylo i usuwamy kolory
-awk 'NR==FNR{a[$0]=1;next}!a[$0]' last.log current.log | sed 's/[\x1B][^m]*m//g' > temp.log
-
-# dopoki tymczasowy plik logu nie jest pusty
-while [[ -s temp.log ]]
+# while temp.log isn't empty...
+while [[ -s tg/temp.log ]]
 do
-  # wycinamy z niego do 30 linii
-  line=$(head -n 30 temp.log)
-  echo -n "$(tail -n +31 temp.log)" > temp.log
-  # escape'ujemy rozne znaki
+  # cut first 30 lines from it and put them to "line" variable
+  line=$(head -n 30 tg/temp.log)
+  echo -n "$(tail -n +31 tg/temp.log)" > tg/temp.log
+  # escaping some chars
   line=${line//\\/\\\\}
   line=${line//\'/\\\'}
   line=${line//\"/\\\"}
 
-  # przesylamy zawartosc na czat ze mna
+  # send everything to the admin chat on Telegram...
   result=$(curl -H "Content-Type: application/json" -d '{"chat_id":138268771,"text":"'"$line"'"}' $tgURL"sendMessage")
-  # i logujemy odpowiedz serwera tg
+  # and log server response (for debugging)
   echo $result | jq . >> tg.log
 
 
-  # przesylamy wiadomosci graczy na czat Wieliczka
+  # choose the lines which contain chat messages
   line=$(echo "$line" | grep "Chat Thread")
   if [ -n "$line" ]
   then
-    # usuwamy niepotrzebne elementy wiadomosci
+	# remove unneeded parts of the message, leave time
     line=$(echo "$line" | sed 's/^\(\[..:..:..\] \)[^<]*</\1</g')
-    # wysylamy do api
-    result=$(curl -H "Content-Type: application/json" -d '{"chat_id":-244449217,"text":"'"$line"'"}' $tgURL"sendMessage")
-    # logujemy odpowiedz serwera
-    echo $result | jq . >> tg.log
+    # send that to the non-admin chat on Telegram
+    result=$(curl -H "Content-Type: application/json" -d '{"chat_id":'"$chatID"',"text":"'"$line"'"}' $tgURL"sendMessage")
+    # log server response (for debugging)
+    echo $result | jq . >> tg/tg.log
   fi
 
 done
 
-# zastepujemy poprzedni log obecnym
-cp current.log last.log
+# replace the previous log with the current log
+cp tg/current.log tg/last.log
 
 
 
 
 
 #
-# przesylanie z telegramu na serwer
+# Telegram -> Spigot server
 #
 
-
-# pobieramy pierwsza wiadomosc z tg
+# get the first message from bot
 json=$(curl $tgURL"getUpdates?limit=1")
 
-# przekazujemy serwerowi wiadomosc i pobieramy nastepne dopoki sie nie skoncza
+# loop which sends the message from bot to the server and gets the next messages
 while (( $(echo $json | jq '.result | length') == 1 ))
 do
 
-  # logujemy pobrany JSON
-  echo $json | jq . >> tg.log
-  # wyciagamy ID wiadomosci
+  # log JSON data (for debugging)
+  echo $json | jq . >> tg/tg.log
+  # get message ID from JSON
   id=$(echo $json | jq -r '.result[].update_id')
-  # wyciagamy tresc
+  # get message content from JSON
   message=$(echo $json | jq -r '.result[].message.text')
 
-  # jezeli wiadomosc posiada tresc
+  # if the meesage has content (if it hasn't it's image for example)
   if [ "$message" != "null" ]
   then
-    # wyciagniecie nicku
+    # get sender's username from JSON
     username=$(echo $json | jq -r '.result[].message.from.username')
-    # dodanie say i nicku do wiadomosci
+    # add "say" and username to the message
     messageToSend="say <<"$username">> "$message
 
-    # jesli to ja, to sprawdzamy czy wiadomosc jest komenda i odpowiednio zmieniamy ja jesli tak
-    if [ "$username" = "wanours" ]
+    # if the message was sent by the admin, check if it's a command...
+    if [ "$username" = "$adminUsername" ]
     then
-      messageToSend=$(echo $messageToSend | sed 's/^say <<wanours>> \///g')
+	  # if it's a command, remove "say", username and slash
+      messageToSend=$(echo $messageToSend | sed 's/^say <<'"$adminUsername"'>> \///g')
     fi
 
-    # przekazanie wiadomosci serwerowi
-    /home/mcserver/various/spigcom.sh $messageToSend
+    # send message to the spigot server through screen, the terminal manager
+    screen -p 0 -S spigot -X eval "stuff "'\042'"$messageToSend"'\042'"\\015"
   fi
 
-  # kolejne pobranie wiadomosci z tg
+  # get the next message from bot
   json=$(curl $tgURL"getUpdates?limit=1&offset="$((id + 1)) )
 
 done
 
 
 
-# czekamy sekunde przed nastepnymi zapytaniami do tg
+# wait a second before sending requests again
 sleep 1
 done
