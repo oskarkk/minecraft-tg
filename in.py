fad34df5b2b77db9374fba73db0abe20865f3abb
oskarkk <16339637+oskarkk@users.noreply.github.com>
2019-03-24 16:09:34 +0100

    Add many fixes and add check for dictionary keys

--- in.py
+++ in.py
@@ -2,34 +2,43 @@ import sys, re, requests
 
 token = sys.argv[1]
 url = 'https://api.telegram.org/bot'+token+'/getUpdates'
-consoleChatID = sys.argv[2]
-chatChatID = sys.argv[3]
+consoleChatID = int(sys.argv[2])
+chatChatID = int(sys.argv[3])
 adminUsername = sys.argv[4]
 
 data = requests.get(url).json()
+
 with open('json.log', 'a') as f:
-  f.write(data)
+  f.write(str(data)+'\n')
+
 updatesNum = len(data['result'])
 
 while updatesNum > 0 :
   for update in data['result']:
-    message = update['message']['text']
+    # go to the next update unless the update is a message and has text
+    try:
+      message = update['message']['text']
+    except KeyError:
+      continue
+
     messageChatID = update['message']['chat']['id']
-    # if the meesage has content (if it hasn't it's image for example)
-    # and it's from admin or message chat on Telegram
-    if message and (messageChatID in [consoleChatID, chatChatID]):
-      username = update['message']['from']['user']
-      for line in message.splitlines():
-        # if the message was sent by the admin and it's a command...
-        if username == adminUsername and line[0] == '/' :
-          line = line[1:]  # remove slash
-        else:
-          line = 'say <<' + username + '>> ' + line
-        print(line+'\n')
+    username = update['message']['from']['username']
+
+    # check if message is from "console" or "chat" chat
+    if messageChatID not in [consoleChatID, chatChatID]:
+      continue
+
+    for line in message.splitlines():
+      # if the message was sent by the admin and it's a command
+      if username == adminUsername and line[0] == '/' :
+        line = line[1:]  # remove slash
+      else:
+        line = 'say <<' + username + '>> ' + line
+      print(line+'\n')
 
   # get the next updates
   lastID = data['result'][-1]['update_id']
   data = requests.get( url+'?offset='+str(lastID + 1) ).json()
   with open('json.log', 'a') as f:
-    f.write(data)
+    f.write(str(data)+'\n')
   updatesNum = len(data['result'])
