924214ccf9ea962fed4e1ae7294ea2c94cbd0aca
oskarkk <16339637+oskarkk@users.noreply.github.com>
2021-01-04 23:20:48 +0100

    Add more logging and adapt to new way of passing commands to spigot

--- telegram.py
+++ telegram.py
@@ -1,11 +1,14 @@
 import requests
 
+def log(text):
+  with open('json.log', 'a') as f:
+    f.write(str(text)+'\n')
+
 def send(token, id, text, parse_mode=0):
   url = 'https://api.telegram.org/bot'+token+'/sendMessage'
   dic = {'chat_id': id, 'text': text}
   if parse_mode: dic['parse_mode'] = parse_mode
-  return requests.post(url, json=dic).json()
+  log(dic)
+  resp = requests.post(url, json=dic).json()
+  log(resp)
 
-def log(text):
-  with open('json.log', 'a') as f:
-    f.write(str(text)+'\n')
